b86fdc486437f1f06bcee07e428437369fbab2e7
yesicamilagros <milagroslc654@gmail.com>
2025-04-07 18:29:33 -0500

    Mensaje descriptivo del cambio

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WhatsappController } from './whatsapp/whatsapp.controller';
 import { WhatsappService } from './whatsapp/whatsapp.service';
-
+import { OpenaiModule } from 'src/openai/openai.module';
 @Module({
+  imports: [OpenaiModule],
   controllers: [WhatsappController],
   providers: [WhatsappService]
 })
